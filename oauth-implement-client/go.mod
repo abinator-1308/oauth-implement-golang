2bfade5833d13dc58dcbe710360579198476a0ff
Abhinav Kumar Singh <abinator.as@gmail.com>
2022-07-17 03:51:22 +0530

    Setting up client to generate JWT token

---
+++ oauth-implement-client/go.mod
@@ -0,0 +1,5 @@
+module oauth-implement-client
+
+go 1.18
+
+require github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
--- oauth-implement-client/src/main.go
+++ oauth-implement-client/src/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	jwt "github.com/golang-jwt/jwt/v4"
+	"time"
+)
+
+var mySignedKey = []byte("sampleSignedKey")
+
+func GenerateJWTToken() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["user"] = "@binator_1308"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(mySignedKey)
+
+	if err != nil {
+		fmt.Errorf("generating JWT Token failed")
+		return "", err
+	}
+
+	return tokenString, nil
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	tokenString, err := GenerateJWTToken()
+	if err != nil {
+		fmt.Println("Error in generating token")
+	}
+
+	fmt.Println(tokenString)
 }
