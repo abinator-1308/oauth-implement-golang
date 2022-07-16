35cff47d9f20c49ab1601697da6a77d25f69ca26
Abhinav Kumar Singh <abinator.as@gmail.com>
2022-07-17 04:37:41 +0530

    Starting a basic server

--- oauth-implement-server/src/main.go
+++ oauth-implement-server/src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Dummy secret information")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
 
 func main() {
 	fmt.Println("Hello Server")
+	handleRequests()
 }
