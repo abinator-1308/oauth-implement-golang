e1b304d4ff6fa293e95e8fbf6ae83c3a6ce3d2e6
Abhinav Kumar Singh <abinator.as@gmail.com>
2022-07-17 04:45:44 +0530

    basic auth

--- oauth-implement-server/src/main.go
+++ oauth-implement-server/src/main.go
@@ -10,8 +10,18 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Dummy secret information")
 }
 
+func isAuthorized(endpoint func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+
+		} else {
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
+}
+
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.Handle("/", isAuthorized(homePage))
 	log.Fatal(http.ListenAndServe(":9000", nil))
 }
 
